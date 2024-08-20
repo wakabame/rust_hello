3021e8d169850a146e362d53a7982217548c2d81
wakabame <wakabame89@gmail.com>
2024-08-20 13:52:31 +0000

    Mutex の lock を使って排他制御を実現する

--- src/thread.rs
+++ src/thread.rs
@@ -1,14 +1,16 @@
-use std::sync::Arc;
+use std::sync::{Arc, Mutex};
 use std::thread;
 
 pub fn main() {
     let mut handles = Vec::new();
-    let mut data = Arc::new(vec![1; 10]);
+    let data = Arc::new(Mutex::new(vec![1; 10]));
 
     for x in 0..10 {
         let data_ref = data.clone();
         handles.push(thread::spawn(move || {
-            data_ref[x] += 1;
+            // lock を使って data への可変参照を得る
+            let mut data = data_ref.lock().unwrap();
+            data[x] += 1;
         }));
     }
 
