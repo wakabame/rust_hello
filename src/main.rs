8f9735a0bcaf098459dc9bcb2e3b2863a3f517b7
wakabame <wakabame89@gmail.com>
2024-08-22 06:40:45 +0000

    clap, builder を用いた記述方式

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,30 @@
-use std::env;
+use clap::{App, Arg};
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    println!("{:?}", args);
+    let matches: clap::ArgMatches = App::new("My RPN Program")
+        .version("1.0.0")
+        .author("Your Name")
+        .about("Super awesome sample RPN caluclator")
+        .arg(
+            Arg::new("formula_file")
+                .about("Formula written in RPN")
+                .index(1)
+                .required(false),
+        )
+        .arg(
+            Arg::new("verbose")
+                .about("Sets the level of vervosity")
+                .short('v')
+                .long("verbose")
+                .required(false),
+        )
+        .get_matches();
+
+    match matches.value_of("formula_file") {
+        Some(file) => println!("File specified: {}", file),
+        None => println!("No file specified."),
+    }
+
+    let verbose = matches.is_present("verbose");
+    println!("Is verbosity specified?: {}", verbose);
 }
